e87ace3bd11751ccc236425da53b929e3b474975
Vinay <vinaydavda2604@gmail.com>
2021-06-28 18:31:59 +0530

    Full working code with working dynamic links and boldness of text

--- pyresumeapp/views.py
+++ pyresumeapp/views.py
@@ -8,12 +8,13 @@ def index(request):
     return render(request, 'index.html')
 
 def renderResume(request):
-    objective = request.GET.get('objective')
     fname = request.GET.get('firstname')
     mname = request.GET.get('middlename')
     lname = request.GET.get('lastname')
     email = request.GET.get('email')
     phone = request.GET.get('phone')
+    githublink = request.GET.get('githublink')
+    linkedinlink = request.GET.get('linkedinlink')
     degree = request.GET.get('degree')
     university = request.GET.get('university')
     cgpa = request.GET.get('cgpa')
@@ -23,30 +24,136 @@ def renderResume(request):
     classx = request.GET.get('classx')
     schoolx = request.GET.get('schoolx')
     percentagex = request.GET.get('percentagex')
-    languages = request.GET.get('languages')
-    frameworks = request.GET.get('frameworks')
-    itconstructs = request.GET.get('itconstructs')
-    dblanguage = request.GET.get('dblanguage')
-    operatingsystems = request.GET.get('operatingsystems')
-    developmenttools = request.GET.get('devlopmenttools')
-    projects = request.GET.get('projects')
-    send_projects = []
-    for i in projects.replace('[bold]', '[bold] ').replace('[/bold]', ' [/bold]').split(',,,'):
-        send_projects.append(i.split(' '))
+    languages = request.GET.get('languages').split(',')
+    send_languages = ''
+    for i in languages:
+        send_languages += i.lstrip().rstrip()
+        if languages.index(i) < len(languages)-1:
+            send_languages += ', '
 
+    frameworks = request.GET.get('frameworks').split(',')
+    send_frameworks = ''
+    for i in frameworks:
+        send_frameworks += i.lstrip().rstrip()
+        if frameworks.index(i) < len(frameworks)-1:
+            send_frameworks += ', '
+
+    itconstructs = request.GET.get('itconstructs').split(',')
+    send_itconstructs = ''
+    for i in itconstructs:
+        send_itconstructs += i.lstrip().rstrip()
+        if itconstructs.index(i) < len(itconstructs)-1:
+            send_itconstructs += ', '
+
+    dblanguage = request.GET.get('dblanguage').split(',')
+    send_dblanguage = ''
+    for i in dblanguage:
+        send_dblanguage += i.lstrip().rstrip()
+        if dblanguage.index(i) < len(dblanguage)-1:
+            send_dblanguage += ', '
+
+    operatingsystems = request.GET.get('operatingsystems').split(',')
+    send_operatingsystems = ''
+    for i in operatingsystems:
+        send_operatingsystems += i.lstrip().rstrip()
+        if operatingsystems.index(i) < len(operatingsystems)-1:
+            send_operatingsystems += ', '
+
+    developmenttools = request.GET.get('devlopmenttools').split(',')
+    send_developmenttools = ''
+    for i in developmenttools:
+        send_developmenttools += i.lstrip().rstrip()
+        if developmenttools.index(i) < len(developmenttools)-1:
+            send_developmenttools += ', '
+
+    familiarwith = request.GET.get('familiarwith').split(',')
+    send_familiarwith = ''
+    for i in familiarwith:
+        send_familiarwith += i.lstrip().rstrip()
+        if familiarwith.index(i) < len(familiarwith)-1:
+            send_familiarwith += ', '
+
+
+    # print("-------->>>", request.GET.get('projects'))
+    projects = request.GET.get('projects').replace('[bold]', '[bold] ').replace('[/bold]', ' [/bold]').replace('[/link]', ' [/link]').split(',,,')
+    
+    # all_other_text = []
+    # all_urls = []
+    send_projects = []
+
+    for project in projects:
+        url_text = []
+        other_text = []
+        for word in project.split():
+            if '[link' in word:
+                end_link = word.index('"]')
+                url_text = word[7:end_link]
+                other_text.append('[link]')
+                other_text.append(word[end_link+2:])
+            else:
+                other_text.append(word)
+
+        # Joining link words starts
+        cp = [] # connection points
+        for word in other_text:
+            if word == '[link]' or word == '[/link]':
+                cp.append(other_text.index(word))
+
+        # other_text[x[3:6] = [''.join(x[3:6])]]
+        other_text[cp[0]+1:cp[1]] = [' '.join(other_text[cp[0]+1:cp[1]])]
+        # Joining link words ends
+            
+        # print('url text : ', url_text)
+        # print('other text : ', other_text)
+
+        send_projects.append({'url_data': url_text, 'text_data': other_text})
+
+    
+
+    # Portfolio operations starts here...
     portfoliolinks = request.GET.get('portfoliolinks')
+    send_portfoliolinks = []
+
+    portfolio_links = portfoliolinks.replace('[bold]', '[bold] ').replace('[/bold]', ' [/bold]').replace('[/link]', ' [/link]').split(',,,')
+
+    send_portfoliolinks = []
+
+    for portfolio_link in portfolio_links:
+        url_text = ''
+        other_text = []
+        for word in portfolio_link.split():
+            if '[link' in word:
+                end_link = word.index('"]')
+                url_text = word[7:end_link]
+                other_text.append('[link]')
+                other_text.append(word[end_link+2:])
+            else:
+                other_text.append(word)
+
+
+        # Joining link words starts
+        cp = [] # connection points
+        for word in other_text:
+            if word == '[link]' or word == '[/link]':
+                cp.append(other_text.index(word))
+
+        other_text[cp[0]+1:cp[1]] = [' '.join(other_text[cp[0]+1:cp[1]])]
+        # Joining link words end
+
+        send_portfoliolinks.append({'url_data': url_text, 'text_data': other_text})
+    
+
     otherdetails = request.GET.get('otherdetails')
     send_other_details = []
     for j in otherdetails.replace('[bold]', '[bold] ').replace('[/bold]', ' [/bold]').split(',,,'):
         send_other_details.append(j.split(' '))
 
-    hobbies = request.GET.get('hobbies')
-
     resume_data = {
-        'fullname': fname+' '+lname,
-        'objective': objective,
+        'fullname': fname.capitalize()+' '+mname[0].upper()+'. '+lname.capitalize(),
         'email': email,
         'phone': phone,
+        'githublink': githublink.lstrip().rstrip(),
+        'linkedinlink': linkedinlink.lstrip().rstrip(),
         'degree': degree,
         'university': university,
         'cgpa': cgpa,
@@ -56,16 +163,18 @@ def renderResume(request):
         'classx': classx,
         'schoolx': schoolx,
         'percentagex': percentagex,
-        'languages': languages.split(','),
-        'frameworks': frameworks.split(','),
-        'itconstructs': itconstructs.split(','),
-        'dblanguages': dblanguage.split(','),
-        'operatingsystems': operatingsystems.split(','),
-        'developmenttools': developmenttools.split(','),
+        'languages': send_languages,
+        'frameworks': send_frameworks,
+        'itconstructs': send_itconstructs,
+        'dblanguages': send_dblanguage,
+        'operatingsystems': send_operatingsystems,
+        'developmenttools': send_developmenttools,
+        'familiarwith': send_familiarwith,
+        # 'projects': all_other_text, # Project text
+        # 'project_urls': all_urls, # Project Urls
         'projects': send_projects,
-        'portfoliolinks': portfoliolinks,
+        'portfoliolinks': send_portfoliolinks,
         'otherdetails': send_other_details,
-        'hobbies': hobbies,
     }
     return render(request, 'renderresume.html', resume_data)
 
@@ -90,10 +199,27 @@ Microsoft Visual Studio, Jupyter Notebook, Pycharm
 Django,Flask,Boorstrap4,Kivy
 MySQL
 Linux,Windows,Mac,Android
-Text Utility app in [bold]Django[/bold] for various operations on text like uppercase-lowercase conversion, character count, etc.,,,Working model of [bold]Hamming Code[/bold] 1 bit Linear Error Correction in Python3,,,[bold]Resume Creator app[/bold] using [bold]Django and Bootstrap4[/bold] which renders a Resume from user entered data
-Github,https://github.com/vinaydavda,,,HackerRank,https://www.hackerrank.com/vinaydavda7,,,Certificates,https://drive.google.com/drive/folders/1jk0ZcvEGGfyF_amEX8ppHGCkqf9ykmpY?usp=sharing
+Flask,C++,Java,JavaScript,PHP,Kivy,Wordpress,Inkscape
+
+
+
+
+
+[bold][link="https://github.com/vinaydavda/Text-Utility"]Text Utility app in Django[/link][/bold] for various operations on text like uppercase-lowercase conversion, character count, etc.,,,
+Working model of [bold][link="https://github.com/vinaydavda/Hamming-Code"]Hamming Code[/link][/bold] 1 bit Linear Error Correction in Python3,,,
+[bold][link="https://github.com/vinaydavda/PyResume-Maker"]Resume Creator app[/link][/bold] using [bold]Django and Bootstrap4[/bold] which renders a Resume from user entered data
+
+
+
+
+[link="https://www.hackerrank.com/vinaydavda7"]HackerRank[/link],,,
+[link="https://drive.google.com/drive/folders/1jk0ZcvEGGfyF_amEX8ppHGCkqf9ykmpY?usp=sharing"]Certificates[/link]
+
+
+
+
+
 Certificate for Completion of Python Training - [bold]IIT Bombay[/bold],,,Learned some basic concepts of [bold]Public-Private keys[/bold] and [bold]End-to-end Encryption[/bold] using Python in College,,,Attended one day workshop on [bold]Python and Machine Learning[/bold] - Smt. K.S.N. Kansagara Mahila College (Rajkot)
-Programming,Sports,Reading,Vector Art
 '''
 
 '''
